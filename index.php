43ad6977a8455d2aa03c5829f3aa1fc0e9c66c26
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-10 10:44:44 +0200

    v2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>partie 7 exercice 2</title>
+        <title>partie 7 exercice 4</title>
         <style>
             form {
                 display: flex;
