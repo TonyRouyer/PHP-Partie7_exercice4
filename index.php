04f08e40489a7a8bfa4293a985d9008d1190cd6c
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-10 10:42:27 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>partie 7 exercice 2</title>
+        <style>
+            form {
+                display: flex;
+                flex-direction: column;
+                width: 50%;
+                margin: 0 auto;
+            }
+            label {
+                margin-top: 10px;
+            }
+            #firstname, #lastname {
+                border: 0px;
+                border-bottom: 1px Solid black;
+            }
+            #sendBtn {
+                width: 10%;
+                margin-top: 10px;
+            }
+        </style>
+    </head>
+    <body>
+        <form action="user.php" method="POST">
+            <label for="firstname">Prénom : <input type="text" id="firstname" name="firstname" /></label>
+            <label for="lastname">Nom : <input type="text" id="lastname" name="lastname" /></label>
+            <input type="submit" id="sendBtn" />
+        </form>
+    </body>
+</html>
---
+++ user.php
@@ -0,0 +1,3 @@
+<?php
+    echo $_POST['lastname'] . ' ' . $_POST['firstname'];
+?>
